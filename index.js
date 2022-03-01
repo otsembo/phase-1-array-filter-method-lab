231398852915d9e2787733ad5eef56012bde4139
otsembo <okumu.otsembo@gmail.com>
2022-03-01 15:22:12 +0300

    commit tree 8f119191def133f7e6cbaebbad4874052a6e8f33

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution here
+function findMatching(drivers, name){
+  return drivers.filter(function (i) {
+    return i.toLowerCase() === name.toLowerCase();
+  })
+}
+
+
+function  fuzzyMatch(persons, name){
+  return persons.filter(function (i) {
+    return i.toLowerCase().substring(0,2) === name.toLowerCase().substring(0,2);
+  })
+}
+
+function matchName(persons, name){
+  return persons.filter(function (i){
+    const actualName = i.name;
+    return actualName.toLowerCase() === name.toLowerCase();
+  });
+}
